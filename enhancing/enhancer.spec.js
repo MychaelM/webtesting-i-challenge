521161d7bed5f9b2f30c68f436e83c4fee5749c9
Mychael M <mychaelbmenges@gmail.com>
2021-01-19 16:39:39 -0700

    added test for repair function and updated repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+
+describe("enhancing unit tests", () => {
+  const item = {
+    name: "item",
+    durability: 15,
+    enhancement: 0
+  }
+
+  it("repairs", () => {
+    expect(enhancer.repair(item)).toEqual({
+      ...item,
+      durability: 100
+    })
+  })
+  
+})
